bcdb7f0c3e254039b5ad72c411e4dc9e1c6057ed
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-08-02 11:19:56 +0530

    Added Seperation Of Concerns

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
-const cookie = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const bcrypt=require('bcrypt');
 const path = require('path');
 const connectDB=require("./config/mongoose-connection")
@@ -10,16 +10,16 @@ const ownerRouter = require('./routes/ownerRouter');
 const productRouter = require('./routes/productRouter');
 const userRouter = require('./routes/userRouter');
 
-
-app.set('view engine', 'ejs');
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 app.use(express.static(path.join(__dirname, "public")))
+app.set('view engine', 'ejs');
 
 connectDB();;
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.send("working")
 })
 
 app.get('/owner',ownerRouter);
